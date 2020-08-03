665548702ec8fe74bacd5b4aa86ed9340fbde531
aubr5652 <augisbraziunas@mif.stud.vu.lt>
2020-08-03 10:30:20 +0300

    firebase

---
+++ public/source/sketch.js
@@ -0,0 +1,38 @@
+var boardSize = 800;
+var game;
+var boardDiv = boardSize/8;
+var selected;
+var player = 1;
+
+var database = fire.database();
+var clicks = 0;
+
+function setup() {
+	createCanvas(boardSize, boardSize);
+	game = new ChessGame(player, boardSize, 0, 0);
+}
+
+function mouseClicked() {
+	
+	database.ref('clicks').set({
+		clickNumber: clicks
+	});
+	clicks++;
+	var sq = getSquare();
+	game.input(sq[0], sq[1]);
+}
+
+var getSquare = function(){
+	var sq = [];
+	var col = Math.floor(mouseX/boardDiv);
+	var row = Math.floor(mouseY/boardDiv);
+	sq.push(row);
+	sq.push(col);
+	return sq;
+}
+
+function draw() {
+	background(255);
+	game.draw();
+}
+
